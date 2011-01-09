f775aa324f65b7eb27da41cb42dbf33c461d6711
Amitaibu <amitai@gizra.com>
2011-01-09 22:36:07 +0200

    Revert last commit.

--- jscrollpane.js
+++ jscrollpane.js
@@ -1,25 +1,23 @@
 // $Id:$
-(function ($) {
+
 /**
  * Add rounded corners.
  */
-Drupal.behaviors.jScrollPane = {
-  attach: function(context){
-    // Set the rounded corners.
-    if (Drupal.settings.jscrollpane.commands) {
-      var jscrollpane = Drupal.settings.jscrollpane.commands;
-      var options = {};
-      // Add the rounded corners to the page.
-      for (var key in jscrollpane) {
-        options = {};
-        // Build the options if exist.
-        if ($(jscrollpane[key]['options']).length > 0) {
-          options = jscrollpane[key]['options'];
-        }
+Drupal.behaviors.jScrollPane = function() {
+  // Set the rounded corners.
+  if (Drupal.settings.jscrollpane.commands) {
+    var jscrollpane = Drupal.settings.jscrollpane.commands;
 
-        $(jscrollpane[key]['selector']).jScrollPane(options);
+    var options = {};
+    // Add the rounded corners to the page.
+    for (var key in jscrollpane) {
+      options = {};
+      // Build the options if exist.
+      if ($(jscrollpane[key]['options']).length > 0) {
+        options = jscrollpane[key]['options'];
       }
+
+      $(jscrollpane[key]['selector']).jScrollPane(options);
     }
   }
 }
-})(jQuery);
