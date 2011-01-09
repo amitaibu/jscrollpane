7cf11025000c31eee5b55f4c4249c7e72b99a037
Amitaibu <amitai@gizra.com>
2011-01-09 22:35:02 +0200

    Upgrade JS.

--- jscrollpane.js
+++ jscrollpane.js
@@ -1,23 +1,25 @@
 // $Id:$
-
+(function ($) {
 /**
  * Add rounded corners.
  */
-Drupal.behaviors.jScrollPane = function() {
-  // Set the rounded corners.
-  if (Drupal.settings.jscrollpane.commands) {
-    var jscrollpane = Drupal.settings.jscrollpane.commands;
+Drupal.behaviors.jScrollPane = {
+  attach: function(context){
+    // Set the rounded corners.
+    if (Drupal.settings.jscrollpane.commands) {
+      var jscrollpane = Drupal.settings.jscrollpane.commands;
+      var options = {};
+      // Add the rounded corners to the page.
+      for (var key in jscrollpane) {
+        options = {};
+        // Build the options if exist.
+        if ($(jscrollpane[key]['options']).length > 0) {
+          options = jscrollpane[key]['options'];
+        }
 
-    var options = {};
-    // Add the rounded corners to the page.
-    for (var key in jscrollpane) {
-      options = {};
-      // Build the options if exist.
-      if ($(jscrollpane[key]['options']).length > 0) {
-        options = jscrollpane[key]['options'];
+        $(jscrollpane[key]['selector']).jScrollPane(options);
       }
-
-      $(jscrollpane[key]['selector']).jScrollPane(options);
     }
   }
 }
+})(jQuery);
